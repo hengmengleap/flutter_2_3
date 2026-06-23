a46056ed9fd4644f65e3104c1617b9271a712cb6
hengmengleap <leapkmeng@gmal.com>
2026-06-23 09:28:51 +0700

    new homework3

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_application_1/home%20page/grid_view_builder.dart';
 import 'package:flutter_application_1/home%20page/gride_view_widget.dart';
 import 'package:flutter_application_1/home%20page/homework.dart';
 import 'package:flutter_application_1/home%20page/homework_2.dart';
+import 'package:flutter_application_1/home%20page/homework_3.dart';
 import 'package:flutter_application_1/home%20page/list_tile_widget.dart';
 import 'package:flutter_application_1/home%20page/list_view_builder.dart';
 import 'package:flutter_application_1/home%20page/list_view_widget.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:AcledaUi(
+      home: Homework3(
         
       ),
     );
